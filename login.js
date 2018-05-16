93689ac5cf89909f6f717b0d0ef817a2a7c581c2
noenadies <33671023+noenadies@users.noreply.github.com>
2018-05-15 20:37:34 -0500

    Update login.js

--- login.js
+++ login.js
@@ -760,7 +760,6 @@ sfinirapnode("taxis",vndhijo,"lngdestino",lngus);
           content: contentString
         });
 
-
   var image="https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/32/Map-Marker-Marker-Inside-Chartreuse.png";
   //4.6284038,-74.1458368, Bogotá 4.628440, -74.137178
            var marker = new google.maps.Marker({
